2b8057e2c67753f35071f6d19956937558515ce3
Nathan-Sasa <Jonathanbikuta05@gmail.com>
2025-05-25 23:43:49 +0100

    mise a jours du text de la page about

--- src/component/about/about.component.ts
+++ src/component/about/about.component.ts
@@ -17,23 +17,23 @@ export class AboutComponent {
       description: 'Je m’appelle Nathan, développeur web passionné par la programmation. '
     },
     {
-      icon : 'ri-account-circle-fill',
-      title: 'About Me',
+      icon : 'ri-git-branch-line',
+      title: 'Parcours',
       description: 'Autodidacte et curieux, j’aime concevoir des interfaces claires, fonctionnelles et élégantes, avec une attention particulière à l’expérience utilisateur.'
     },
     {
-      icon : 'ri-account-circle-fill',
-      title: 'About Me',
+      icon : 'ri-emotion-happy-line',
+      title: 'Motivation',
       description: 'passionné par la création d’applications web dynamiques et réactives. J’aime concevoir des interfaces claires, fonctionnelles et élégantes, avec une attention particulière à l’expérience utilisateur.'
     },
     {
-      icon : 'ri-account-circle-fill',
-      title: 'About Me',
-      description: 'I am a web developer with a passion for creating dynamic and responsive web applications.'
+      icon : 'ri-tools-fill',
+      title: 'Compétences',
+      description: 'Je maîtrise les technologies web modernes telles que HTML, CSS, JavaScript, Angular et Node.js. J’ai également de l’expérience avec des outils de gestion de version comme Git.'
     },
     {
-      icon : 'ri-account-circle-fill',
-      title: 'About Me',
+      icon : 'ri-user-search-line',
+      title: 'recherches',
       description: 'I am a web developer with a passion for creating dynamic and responsive web applications.'
     },
     {
